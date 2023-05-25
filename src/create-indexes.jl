d7059c07b1d69945b4cec2a665b2bcd025c3874d
Eric S. Tellez <donsadit@gmail.com>
2023-05-24 21:49:03 -0500

    working

--- src/create-indexes.jl
+++ src/create-indexes.jl
@@ -2,60 +2,19 @@ using Glob, TextSearch, InvertedFiles, SimilaritySearch,
     JLD2, JSON, CategoricalArrays, Base64, DataFrames,
     Parquet2, Markdown
 
-function one_package(metafile)
-    M = JSON.parse(read(metafile, String))
-    dn = dirname(metafile)
-    R = JSON.parse(read(joinpath(dn, "Readme.json"), String))
-    readme = String(base64decode(R["content"]))
-    license = get(M, "license", nothing)
-    license = license === nothing ? "" : get(license, "name", "")
-    description = get(M, "description", nothing)
-    description = description === nothing ? "" : description
-    topics = get(M, "topics", nothing)
-    topics = topics === nothing ? "" : join(topics, ' ')
-    url = M["html_url"]
-    #name = replace(basename(url), r".jl$" => "")
-    name = basename(dn)
-    stargazers_count = get(M, "stargazers_count", 0)
-    (; name, url, description, topics, readme, license, stargazers_count)
-end
-
-function package_dataset()
-    D = DataFrame(
-                  name = String[],
-                  url = String[],
-                  description = String[],
-                  topics = String[],
-                  readme = String[],
-                  license = String[],
-                  stargazers_count = Int32[]
-                 )
-
-    for metafile in glob("PackageData/*/*/Metadata.json")
-        try
-            data = one_package(metafile)
-            push!(D, data)
-        catch e
-            @info "ERROR while loading data from $(dirname(metafile)), ignoring package"
-            @info e 
-        end
-    end
-    
-    D
-end
-
 function package_data(D, id, weight)
     r = D[id, :]
-    desc = length(r.description) > 0 ? r.description : ""
-    topics = length(r.topics) > 0 ? r.topics : ""
+    
+    subdir = length(r.subdir) > 0 ? string("/", subdir) : ""
+    url = string(r.repo, subdir)
     Dict(
         "id" => id,
         "name" => r.name,
-        "description" => desc,
-        "topics" => topics,
-        "url" => r.url,
+        "url" => url,
+        "description" => r.description,
+        "topics" => r.topics,
         "score" => round(abs(weight), digits=2),
-        "gh-stars" => r.stargazers_count
+        "stars" => r.stars
     )
 end
 
@@ -71,32 +30,24 @@ function search_packages(D, idx, query, k)
     R
 end
 
-function load_or_create_indexes(; datadir="data")
+function load_or_create_indexes(Pkgs; datadir="data")
     mkpath(datadir)
-    dataset_file = joinpath(datadir, "packages.parquet")
     nameindex_file = joinpath(datadir, "nameidx.jld2")
     descindex_file = joinpath(datadir, "descidx.jld2")
     readmeindex_file = joinpath(datadir, "readmeidx.jld2")
-
-    if isfile(dataset_file)
-        D = DataFrame(Parquet2.Dataset(dataset_file), copycols=false)
-    else
-        D = package_dataset()
-        Parquet2.writefile(dataset_file, D)
-    end
-    
+ 
     if isfile(nameindex_file)
         nameidx, _ = loadindex(nameindex_file; staticgraph=true)
     else
-        nameidx = BM25InvertedFile(TextConfig(qlist=[3]), D.name)
-        append_items!(nameidx, D.name)
+        nameidx = BM25InvertedFile(TextConfig(qlist=[3]), Pkgs.name)
+        append_items!(nameidx, Pkgs.name)
         saveindex(nameindex_file, nameidx)
     end
     
     if isfile(descindex_file)
         descidx, _ = loadindex(descindex_file; staticgraph=true)
     else
-        corpus = [string(a, " ", b) for (a, b) in zip(D.description, D.topics)]
+        corpus = [string(a, " ", b) for (a, b) in zip(Pkgs.description, Pkgs.topics)]
         descidx = BM25InvertedFile(TextConfig(nlist=[1], qlist=[4]), corpus) do t
             3 <= t.ndocs <= 1000
         end
@@ -107,14 +58,14 @@ function load_or_create_indexes(; datadir="data")
     if isfile(readmeindex_file)
         readmeidx, _ = loadindex(readmeindex_file; staticgraph=true)
     else
-        readmeidx = BM25InvertedFile(TextConfig(nlist=[1]), D.readme) do t
+        readmeidx = BM25InvertedFile(TextConfig(nlist=[1]), Pkgs.readme) do t
             3 <= t.ndocs <= 1000
         end
-        append_items!(readmeidx, D.readme)
+        append_items!(readmeidx, Pkgs.readme)
         saveindex(readmeindex_file, readmeidx)
     end
 
-    (; D, nameidx, descidx, readmeidx)
+    (; Pkgs, nameidx, descidx, readmeidx)
 end
 
 #=
---
+++ src/run-fetch.jl
@@ -0,0 +1,154 @@
+using TOML, JSON, DataFrames, CSV, Dates
+using Downloads: download, RequestError
+
+include("parse-registry.jl")
+
+#=function fetch_readme_from_github(url, version; verbose=false)
+    url = replace(url, "github.com" => "raw.githubusercontent.com")
+    readmeurl = joinpath(url, "v" * version, "README.md") 
+    verbose && (@show readmeurl)
+    buff = download(readmeurl, IOBuffer(); verbose=false)  # keep this as false unless something fails with the http connection 
+    String(take!(buff))
+end=#
+
+function fetch_meta_from_github(repo, subdir, version; verbose=false)
+    url = string(repo, "/", subdir) 
+    verbose && (@show url)
+    buff = download(url, IOBuffer(); verbose=false)
+    page = String(take!(buff))
+    topics = String[]
+    for m in eachmatch(r"/topics/([^\"]+)", page)
+        push!(topics, m.captures[1])
+    end
+
+    stars = 0
+    readme = ""
+
+    description = match(r"""<meta name="description" content="(.+?)">""", page).captures[1]
+    m = match(r"<span.*?>(\d+)</span>\s*?stars", page)
+    m !== nothing && (stars = parse(Int, m.captures[1]))
+    m = match(r"<article.*?>(.+)</article>"s, page)
+    if m !== nothing
+        readme = replace(m.captures[1], r"<.+?>" => "", "&nbsp;" => "\n", "&gt;" => ">", "&lt;" => "<")
+    end
+
+    description, join(topics, ';'), readme, stars
+end
+
+function fetch_meta_from_gitlab(repo, subdir, version; verbose=false)
+    if occursin("github.com", repo)
+        url = replace(repo, "github.com" => "raw.githubusercontent.com")
+        if subdir != ""
+            subdir = string(subdir, "/")
+        end
+        url = string(url, "/", version, "/", subdir, "README.md") 
+    elseif occursin("gitlab.com", repo)
+        url = string(repo, "/-/raw/", version, "/", "README.md") 
+    else
+        url = string(repo, "/", "README.md") 
+    end
+
+    verbose && (@info repo => url)
+    buff = download(url, IOBuffer(); verbose=false)  # keep this as false unless something fails with the http connection 
+    verbose && (@info repo => "OK")
+    readme = String(take!(buff)) 
+    stars = -1
+    topics = ""
+    description = ""
+    description, topics, readme, stars
+end
+
+function fetch_package_registry(row; verbose=false)
+    description = ""
+    topics = ""
+    readme = ""
+    stars = -1
+    statusmeta = 200
+
+    if occursin("github.com", row.repo)
+        try
+            description, topics, readme, stars = fetch_meta_from_github(row.repo, row.subdir, row.version; verbose)
+        catch err
+            if err isa RequestError
+                @info "ignoring metadata from $(row.name) / $(row.repo)"
+                statusmeta = err.response.status
+            else
+                rethrow()
+            end
+        end
+    else
+        for version in ["master", string("v", row.version), "main"]
+            try
+                description, topics, readme, stars = fetch_meta_from_gitlab(row.repo, row.subdir, version; verbose)
+                break
+            catch err
+                if err isa RequestError
+                    @info "retry readme $(row.name) / $(row.repo) - $version"
+                    statusmeta = err.response.status
+                else
+                    rethrow()
+                end
+            end
+        end
+    end
+
+
+    (; row.name, row.uuid, row.repo, row.subdir, row.version, fetched=Dates.now(), statusmeta, description, topics, readme, stars)
+end
+
+
+function update(registrypath, prevfile; verbose=false)
+    updatedreg = parse_registry(registrypath)
+    prev = load_meta_dataframe(prevfile)
+    D = create_meta_dataframe()
+    idxprev = Dict(name => i for (i, name) in enumerate(prev.name))
+   
+    n = length(updatedreg)
+    count = 1
+    for (name, reg) in updatedreg
+        i = get(idxprev, name, 0)
+        @info "precessing $count / $n - $name ($i)"
+        count += 1
+        if i == 0
+            newreg = fetch_package_registry(reg; verbose)
+            push!(D, newreg)
+            continue
+        end
+
+        row = prev[i, :]
+        if row.statusmeta in (200, 404)
+            push!(D, row)
+        else
+            push!(D, fetch_package_registry(reg; verbose))
+        end
+    end
+
+    D
+end
+
+function load_meta_dataframe(filename)
+    CSV.read(filename, DataFrame, missingstring="XXXXXXX", types=Dict(:fetched => DateTime, :statusmeta => Int, :stars => Int))
+end
+
+function create_meta_dataframe()
+    DataFrame(name=String[], uuid=String[], repo=String[], subdir=String[], version=String[], fetched=DateTime[], statusmeta=Int[], description=String[], topics=String[], readme=String[], stars=Int[])
+end
+
+function create(
+        registry;
+        verbose = true
+    )
+    
+    D = create_meta_dataframe()
+    Packages = parse_registry(registry)
+
+    numpackages = length(Packages)
+    for (i, (name, record)) in enumerate(Packages)
+        @info "$i of $numpackages -- $(record.name) - $(record.repo) - $(Dates.now())"
+        reg = fetch_package_registry(record; verbose)
+        push!(D, reg)
+        #rand() < 0.1 && break
+    end
+
+    D
+end
